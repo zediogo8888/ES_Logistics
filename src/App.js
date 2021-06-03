03a0c7ab33602b60d4d6409a2f80be5b1f2ec818
Ze <zediogo88>
2021-06-03 23:17:42 +0100

    reactv01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+  
+import React, {useState, useRef} from 'react';
+import {Container, Card, CardContent, makeStyles, Grid, Button} from '@material-ui/core';
+import QrReader from 'react-qr-reader';
+
+//npm install react-qr-reader
+//npm install @material-ui/core
+function App() { 
+  const [scanResultFile, setScanResultFile] = useState('');
+  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const classes = useStyles();
+  const qrRef = useRef(null);
+
+
+  
+  const handleErrorFile = (error) => {
+    console.log(error);
+  }
+  const handleScanFile = (result) => {
+      if (result) {
+          setScanResultFile(result);
+      }
+  }
+  const onScanFile = () => {
+    qrRef.current.openImageDialog();
+  }
+  const handleErrorCamera= (error) => {
+    console.log(error);
+  }
+  const handleScanCamera = (result) => {
+    if (result){
+        setScanResultWebCam(result);
+    }
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.conatiner}>
+          <Card>
+              <h2 className={classes.title}>Logistics ES Company</h2>
+              <CardContent>
+                  <Grid container spacing={2}>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                        <Button className={classes.btn} variant="contained" onClick={onScanFile}>Scan Qr Code</Button>
+                        <QrReader
+                          ref={qrRef}
+                          delay={300}
+                          style={{width: '100%'}}
+                          onError={handleErrorFile}
+                          onScan={handleScanFile}
+                          legacyMode
+                        />
+                        <h3>Scanned Code URL: {scanResultFile}</h3>
+                      </Grid>
+                      <Grid item xl={10} lg={4} md={6} sm={12} xs={12}>
+                         
+                         <h2 className={classes.h1}>Qr Code Scan by Camera</h2>
+                         <QrReader
+                         delay={300}
+                         style={{width: '100%'}}
+                         onError={handleErrorCamera}
+                         onScan={handleScanCamera}
+                         />
+                         <h3>Scanned By Camera URL: {scanResultWebCam}</h3>
+                      </Grid>
+                  </Grid>
+              </CardContent>
+          </Card>
+    </Container>
   );
+  
 }
+
+
+const useStyles = makeStyles((theme) => ({
+    conatiner: {
+      marginTop: 'auto'
+    },
+    title: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems:  'center',
+      background: '#3366ff',
+      color: '#fff',
+      padding: 20
+    },
+    btn : {
+      font_family: "Helvetica",
+      font_size: 28,
+      marginTop: 10,
+      marginBottom: 20,
+      color: "#ffffff",
+      background: '#002266',
+    },
+    h1 : {
+      font_family: "Helvetica",
+      font_size: 28,
+      marginTop: 10,
+      marginBottom: 30,
+      marginLeft: 50,
+      justifyContent: 'center',
+      color: "#002266",
+      //background: '#002266',
+      
+      
+   }
+}));
 
 export default App;
